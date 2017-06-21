ef026b372d02c0777e561cc505b9323d10acc996
Kyschel <Kyschel@users.noreply.github.com>
2017-06-21 14:39:52 +0200

    Final version, probably

--- startattack.lua
+++ startattack.lua
@@ -1,7 +1,7 @@
 -- startattack.lua
 --
 -- Author: Kyschel
--- Description: Locates player attack action on actionbar and starts/stops attack on command (/startattack, /stopattack)
+-- Description: Locates player attack action on actionbar and starts/stops attack on command
 --
 -- GlobalVariablesPerCharacter from startattack.toc:
 -- AttackActionSlot
@@ -9,28 +9,22 @@
 SLASH_STARTATTACK1 = "/startattack"
 SLASH_STOPATTACK1 = "/stopattack"
 
-local function verifyAttackSlot(s)
-    return IsAttackAction(s)
-end
-
 local function findAttackSlot()
     for i=1,120 do
-        if verifyAttackSlot(i) then
+        if IsAttackAction(i) then
             return i
         end
     end
 end
 
 local function startAttack(start)
-    if not AttackActionSlot or not verifyAttackSlot(AttackActionSlot) then
-        AttackActionSlot = findAttackSlot()
-        
+    if not AttackActionSlot or not IsAttackAction(AttackActionSlot) then
+        AttackActionSlot = findAttackSlot()        
         if not AttackActionSlot then
             DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFFstartattack|r: No attack action found on your actionbars!")
             return
-        end  
-    end   
-    
+        end
+    end    
     if (start and not IsCurrentAction(AttackActionSlot)) or (not start and IsCurrentAction(AttackActionSlot)) then
         UseAction(AttackActionSlot)
     end
